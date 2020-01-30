7637f529fa1e81dceeb45943415483fc34671946
FreedomWriter <natalienetworks@gmail.com>
2020-01-29 23:13:07 -0600

    get, get by id, post work

--- routes/carsRouter.js
+++ routes/carsRouter.js
@@ -13,4 +13,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const car = await db("cars")
+      .where("id", req.params.id)
+      .first();
+    res.json(car);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get car" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { body } = req;
+  console.log(body);
+  try {
+    const [id] = await db("cars").insert(body);
+    res.json(await db("cars").where("id", id));
+  } catch (err) {
+    res.status(500).json({ message: "Failed add car", error: err.message });
+  }
+});
+
 module.exports = router;
